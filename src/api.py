7bb984c85a2e1b83d63718d063973196dd01c368
ursinedeity <huanan1991@gmail.com>
2017-04-21 17:51:06 -0700

    fix string saving issue

--- src/api.py
+++ src/api.py
@@ -384,7 +384,7 @@ class contactmatrix(object):
         h5f.attrs["version"] = __version__
         h5f.attrs["nbins"] = len(self.index)
         ggrp = h5f.create_group("genome")
-        ggrp.create_dataset("assembly",data=self.genome.assembly, compression=compression,compression_opts=compression_opts)
+        ggrp.create_dataset("assembly",data=self.genome.assembly)
         ggrp.create_dataset("chroms",data=self.genome.chroms, compression=compression,compression_opts=compression_opts)
         ggrp.create_dataset("origin",data=self.genome.origin, compression=compression,compression_opts=compression_opts)
         ggrp.create_dataset("length",data=self.genome.length, compression=compression,compression_opts=compression_opts)
