de79fcbb19750e0eb68f371788c762a06e7d00b9
Guido Polles <polles@usc.edu>
2018-03-29 13:06:39 -0700

    added tests folder

---
+++ test/test_utils.py
@@ -0,0 +1,41 @@
+from alabtools.utils import Genome, Index
+import h5py
+import os
+
+def test_genome_io():
+    g = Genome('hg38')
+    with h5py.File('./teststr.h5', 'w') as f:
+        g.save(f)
+    with h5py.File('./teststr.h5', 'r') as f:
+        g2 = Genome(f)
+    os.remove('./teststr.h5')
+    assert g == g2
+
+def test_index_io():
+    with open('testindex5.txt', 'w') as f:
+        f.write('#comment line\n')
+        f.write('chr1 0 1000 gap 0\n')
+        f.write('chr3 0 1000 domain 2\n')
+
+    # load without genome
+    i = Index('testindex5.txt')
+    with h5py.File('./teststr.h5', 'w') as f:
+        i.save(f)
+    with h5py.File('./teststr.h5', 'r') as f:
+        ii = Index(f)
+    assert ii == i
+
+    # load with genome
+    i = Index('testindex5.txt', genome='hg38')
+    with h5py.File('./teststr.h5', 'w') as f:
+        i.save(f)
+    with h5py.File('./teststr.h5', 'r') as f:
+        ii = Index(f)
+    assert ii == i
+
+    os.remove('./teststr.h5')
+    os.remove('./testindex5.txt')
+
+
+test_genome_io()
+test_index_io()
