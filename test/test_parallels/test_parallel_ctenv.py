f51bcda156779481f6928dfaf3d4f04e7d9b58a8
Francesco Musella <fmusella@login2.hoffman2.idre.ucla.edu>
2023-04-21 18:02:17 -0700

    no modifications

--- test/test_parallels/test_parallel_ctenv.py
+++ test/test_parallels/test_parallel_ctenv.py
@@ -4,16 +4,28 @@ import os
 from alabtools import CtFile
 from alabtools import CtEnvelope
 
+# print working directory
+print('\n')
+print('Current working directory:')
+print(os.getcwd())
+print('\n\n')
+
+# print file directory
+print('\n')
+print('File directory:')
+print(os.path.dirname(os.path.realpath(__file__)))
+print('\n\n')
+
 # load the ct file
 ct_name = 'takei_comb.ct'
-ct_name = os.path.join(os.getcwd(), ct_name)
+ct_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), ct_name)
 ct = CtFile(ct_name)
 print(ct.coordinates.shape)
 ct.close()
 
 # create the ctenv file
 ctenv_name = 'takei_comb.ctenv'
-ctenv_name = os.path.join(os.getcwd(), ctenv_name)
+ctenv_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), ctenv_name)
 ctenv = CtEnvelope(ctenv_name, 'w')
 
 # define the ctenv configuration for the run
@@ -21,7 +33,9 @@ config = {'ct_name': ct_name,
           'parallel': {'controller': 'ipyparallel'},
           'fit parameters': {'alpha': 0.0005,
                              'force': False,
-                             'delta_alpha': 0.0001}}
+                             'delta_alpha': 0.0001,
+                             'thresh': 1100,
+                             'min_neigh': 20}}
 
 # run the ctenv calculation
 t1 = time.time()
