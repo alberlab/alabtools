249061a889810a8a7a9030f6713bd068dad83d10
Francesco Musella <fmusella@login3.hoffman2.idre.ucla.edu>
2023-05-02 02:07:58 -0700

    changed version in setup.py

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ clscripts = [
 cmdclass.update({'build_ext': build_ext})
 setup(
     name='alabtools',
-    version='1.1.0',
+    version='1.1.0+bugfix_import',
     author='Nan Hua, Francesco Musella',
     author_email='nhua@usc.edu',
     url='https://github.com/alberlab/alabtools',
