2eb4ace5b4d2803d9cfe1e60d480a9966ea00f54
Francesco Musella <fmusella@login3.hoffman2.idre.ucla.edu>
2023-05-02 03:16:20 -0700

    updated tests to match new imports

--- test/test_parallels/test_parallel_ctenv.py
+++ test/test_parallels/test_parallel_ctenv.py
@@ -1,36 +1,38 @@
 import numpy as np
 import time
 import os
-from alabtools import CtFile
-from alabtools import CtEnvelope
+from alabtools.imaging import CtFile
+from alabtools.imaging import CtEnvelope
 
-# print working directory
 print('\n')
-print('Current working directory:')
-print(os.getcwd())
-print('\n\n')
 
-# print file directory
-print('\n')
-print('File directory:')
-print(os.path.dirname(os.path.realpath(__file__)))
-print('\n\n')
+# print working directory and file directory
+print('Current working directory:\n{}\n\n'.format(os.getcwd()))
+print('File directory:\n{}\n\n'.format(os.path.dirname(os.path.realpath(__file__))))
+
+# change working directory to file directory
+print('Changing working directory to file directory...\n\n')
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# print working directory and file directory
+print('Current working directory:\n{}\n\n'.format(os.getcwd()))
+print('File directory:\n{}\n\n'.format(os.path.dirname(os.path.realpath(__file__))))
 
 # load the ct file
-ct_name = 'takei_comb.ct'
+ct_name = 'takei_rep1.ct'
 ct_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), ct_name)
 ct = CtFile(ct_name)
 print(ct.coordinates.shape)
 ct.close()
 
 # create the ctenv file
-ctenv_name = 'takei_comb.ctenv'
+ctenv_name = 'takei_rep1.ctenv'
 ctenv_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), ctenv_name)
 ctenv = CtEnvelope(ctenv_name, 'w')
 
 # define the ctenv configuration for the run
 config = {'ct_name': ct_name,
-          'parallel': {'controller': 'ipyparallel'},
+          'parallel': {'controller': 'serial'},
           'fit parameters': {'alpha': 0.0005,
                              'force': False,
                              'delta_alpha': 0.0001,
