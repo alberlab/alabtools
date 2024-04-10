4c9bca09a531e38a8247661302059bbdcb66c065
Francesco Musella <fmusella@g.ucla.edu>
2024-04-10 15:37:45 -0700

    version changed to 1.1.21+add_consec

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ clscripts = [
 cmdclass.update({'build_ext': build_ext})
 setup(
     name='alabtools',
-    version='1.1.21',
+    version='1.1.21+add_consec',
     author='Nan Hua, Francesco Musella',
     author_email='nhua@usc.edu',
     url='https://github.com/alberlab/alabtools',
