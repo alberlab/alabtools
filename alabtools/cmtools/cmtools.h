958056b903ac2fe917761bc39200f0c606d80b95
Francesco Musella <fmusella@g.ucla.edu>
2025-12-27 09:44:36 -0800

    cmtools massively simplified, only TopmeanSummaryMatrix is kept (used for the iterative scaling in the HiC processing)

--- alabtools/cmtools/cmtools.h
+++ alabtools/cmtools/cmtools.h
@@ -7,19 +7,3 @@ void TopmeanSummaryMatrix(int * Ap,
                           int * Bi,
                           int * Bj,
                           float * Bx);
-void BuildContactMap(float * coordinates,
-                     int nbead,
-                     int nstruct,
-                     float * radii,
-                     float contactRange,
-                     int DimB,
-                     int * Bi,
-                     int * Bj,
-                     float * Bx);
-void PixelConfidence(float * matrix, int row, int col, //matrix and size
-                     float * confidence,
-                     float * expected);
-
-void TomogramsFromStructure(float * coordinates, int nbead,
-                            float * radii, float * values, float radialExpansion, float sratio,
-                            float * Tomogram, int DimA, int DimB, int DimC);
