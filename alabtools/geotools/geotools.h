64575f98143cb2c72ec1b9d55d75af33aa135354
Francesco Musella <fmusella@g.ucla.edu>
2025-12-27 09:43:54 -0800

    geotools removed

--- alabtools/geotools/geotools.h
+++
@@ -1,5 +0,0 @@
-#include <vector>
-std::vector<double> ConvexHullIntersection(double * A, int ASize,
-                                           double * B, int BSize);
-                          
-void bounding_spheres(float* crd, float* radii, int n_bead, int n_struct, float* results);
