96ed25ee77a0e98cd559fb46e1c8934e84963d92
Francesco Musella <fmusella@g.ucla.edu>
2026-01-14 12:06:10 -0800

    version changed to 1.1.28+fix_toml

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ clscripts = [
 cmdclass.update({'build_ext': build_ext})
 setup(
     name='alabtools',
-    version='1.1.28',
+    version='1.1.28+fix_toml',
     author='Nan Hua, Francesco Musella',
     author_email='nhua@usc.edu',
     url='https://github.com/alberlab/alabtools',
